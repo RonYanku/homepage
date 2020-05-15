eabd7b27f03d575ad9da8bdcbbd559d5c6689e96
Ron Yanku <ron.yanku86@gmail.com>
2020-05-15 18:24:35 +0300

    Updates

---
+++ precache-manifest.61d281604700bc60a8045ec1bd714729.js
@@ -0,0 +1,90 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f92a15e12fe940b007dd6186a54171be",
+    "url": "/homepage/index.html"
+  },
+  {
+    "revision": "168ab7642c0f28d11d2f",
+    "url": "/homepage/static/css/2.7101a1b0.chunk.css"
+  },
+  {
+    "revision": "f18ecce29f7b134278d6",
+    "url": "/homepage/static/css/main.c190e1f9.chunk.css"
+  },
+  {
+    "revision": "168ab7642c0f28d11d2f",
+    "url": "/homepage/static/js/2.30d2ab7c.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/homepage/static/js/2.30d2ab7c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f18ecce29f7b134278d6",
+    "url": "/homepage/static/js/main.72b2a11c.chunk.js"
+  },
+  {
+    "revision": "494aea1241a78459597e",
+    "url": "/homepage/static/js/runtime-main.f1af4ede.js"
+  },
+  {
+    "revision": "7aca843983c29553b148af9cc73d8f22",
+    "url": "/homepage/static/media/SauceEdgeAuth.7aca8439.jpg"
+  },
+  {
+    "revision": "2bc4a96856c38e2a1f3ace3750062afe",
+    "url": "/homepage/static/media/SauceEdgeMenu.2bc4a968.jpg"
+  },
+  {
+    "revision": "0a9afb1ab22d58caf0847247416021fe",
+    "url": "/homepage/static/media/SauceEdgeOrderCon.0a9afb1a.jpg"
+  },
+  {
+    "revision": "b55f49d715dff9a2bd4a54b510fc8a89",
+    "url": "/homepage/static/media/SauceEdgePayment.b55f49d7.jpg"
+  },
+  {
+    "revision": "ff00616381ad7538bc23ade2a00e53cf",
+    "url": "/homepage/static/media/SauceEdgeSideDrawer.ff006163.jpg"
+  },
+  {
+    "revision": "c315b6194bcf6a8a7f679b6910dc1694",
+    "url": "/homepage/static/media/bg.c315b619.jpg"
+  },
+  {
+    "revision": "27897f18ca89be7d4be046ae21a36fd4",
+    "url": "/homepage/static/media/eatseasy1.27897f18.jpg"
+  },
+  {
+    "revision": "420eb787bcbdf63f64faa9d16efda45f",
+    "url": "/homepage/static/media/eatseasy2.420eb787.jpg"
+  },
+  {
+    "revision": "c196001b30b694f7765ed5ff020360e6",
+    "url": "/homepage/static/media/eatseasy3.c196001b.jpg"
+  },
+  {
+    "revision": "10b6efb22384aef8ce7de1305133df70",
+    "url": "/homepage/static/media/eatseasy4.10b6efb2.jpg"
+  },
+  {
+    "revision": "11cad326981a0fb71de26a7487179513",
+    "url": "/homepage/static/media/eatseasy5.11cad326.jpg"
+  },
+  {
+    "revision": "f96b5bd22328d99cfed90030841ba7db",
+    "url": "/homepage/static/media/portfolio1.f96b5bd2.jpg"
+  },
+  {
+    "revision": "91cc680aa12199f721b54bf0a6baef32",
+    "url": "/homepage/static/media/projects-bg.91cc680a.jpg"
+  },
+  {
+    "revision": "becff13b2adfbf4d1c639bd9e1eaeabc",
+    "url": "/homepage/static/media/vision1.becff13b.jpg"
+  },
+  {
+    "revision": "ef2f14184c55b7272d878f8fbf6a1885",
+    "url": "/homepage/static/media/vision2.ef2f1418.jpg"
+  }
+]);
