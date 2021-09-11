6d6892818f3ab1c788a71e1d564fb3184e3af70f
Ron Yanku <ron.yanku86@gmail.com>
2021-09-12 00:33:30 +0300

    whatever

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
       <PortfolioTitle />
       <PortfolioAbout />
       <PortfolioExperience />
-      <PortfolioProjects />
+      {/* <PortfolioProjects /> */}
       <PortfolioContact />
     </div>
   )
